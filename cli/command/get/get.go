f98a7ee23c838dd751f2c9a66100c5c983227827
Darko Križić <darko@krizic.net>
2025-12-29 02:34:30 +0100

    Get works

--- cli/command/get/get.go
+++ cli/command/get/get.go
@@ -2,10 +2,27 @@ package get
 
 import (
 	"context"
+	"log/slog"
 
+	"github.com/dkrizic/feature/cli/command"
+	feature "github.com/dkrizic/feature/cli/repository/feature/v1"
 	"github.com/urfave/cli/v3"
 )
 
 func Get(ctx context.Context, cmd *cli.Command) error {
-	return nil
+	fc, err := command.FeatureClient(cmd)
+	if err != nil {
+		return err
+	}
+
+	key := cmd.StringArg("key")
+
+	slog.Info("Getting feature", "key", key)
+	result, err := fc.Get(ctx, &feature.Key{
+		Name: key,
+	})
+	if err == nil {
+		cmd.Writer.Write([]byte(result.Name + "\n"))
+	}
+	return err
 }
